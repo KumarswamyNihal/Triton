8960c59244b367c2b988f44377aeb187fac573ae
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-12-29 20:16:28 +0530

    changed makefile

--- pi/main.cpp
+++ pi/main.cpp
@@ -6,9 +6,40 @@
 #include <termios.h>
 
 #include "chat_client.h"
+#include "logger.h"
 
 #define PACKET_START_BYTE  0xAA
+int validatepacket(int packetSize, unsigned char *buffer)
+{
+	if(buffer[0] != PACKET_START_BYTE)
+	{
+		printf("VALIDATE BIG%c\n", buffer[0]);
+		return 0;
 
+	}
+		
+	if(buffer[1] != packetSize)
+	{
+		
+		return 0;
+	}
+
+	if(buffer == NULL)
+	{
+		
+		return 0;
+	}
+	unsigned char checksum = 0x00;
+	for(unsigned int i=0; i < packetSize-1; i++)
+		checksum = checksum ^ buffer[i];
+	
+	if(buffer[packetSize-1] != checksum){
+		printf("%c", checksum);
+		return 0; 
+	}
+
+	return 1;
+}
 int main(int argc, char* argv[])
 {
     int ser_dev, count = 0, packetSize = 0;
@@ -73,8 +104,7 @@ int main(int argc, char* argv[])
 					if(count >= packetSize){
 						if(validatepacket(packetSize, buffer) == 1){
 							//Received a packet
-							//printf("%d\n", buffer[1]);
-							 chat_message msg;
+							chat_message msg;
       						msg.body_length(packetSize);
       						strncpy(msg.body(), (const char*)buffer, packetSize);
 							//printf("buffer is: %c\n", buffer[4]);
