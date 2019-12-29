24df2d264561fe92037d821b5e50ee84abc6fa86
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-12-29 12:59:01 +0530

    some structural changes

--- pi/chat_client.cpp
+++ pi/chat_client.cpp
@@ -1,13 +1,3 @@
-//
-// chat_client.cpp
-// ~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <cstdlib>
 #include <deque>
 #include <iostream>
@@ -15,14 +5,11 @@
 #include "asio.hpp"
 #include "chat_message.hpp"
 
+#include "chat_client.h"
+
 using asio::ip::tcp;
 
-typedef std::deque<chat_message> chat_message_queue;
-
-class chat_client
-{
-public:
-  chat_client(asio::io_context& io_context,
+chat_client::chat_client(asio::io_context& io_context,
       const tcp::resolver::results_type& endpoints)
     : io_context_(io_context),
       socket_(io_context)
@@ -30,7 +17,7 @@ public:
     do_connect(endpoints);
   }
 
-  void write(const chat_message& msg)
+  void chat_client::write(const chat_message& msg)
   {
     asio::post(io_context_,
         [this, msg]()
@@ -44,13 +31,12 @@ public:
         });
   }
 
-  void close()
+  void chat_client::close()
   {
     asio::post(io_context_, [this]() { socket_.close(); });
   }
 
-private:
-  void do_connect(const tcp::resolver::results_type& endpoints)
+  void chat_client::do_connect(const tcp::resolver::results_type& endpoints)
   {
     asio::async_connect(socket_, endpoints,
         [this](std::error_code ec, tcp::endpoint)
@@ -62,7 +48,7 @@ private:
         });
   }
 
-  void do_read_header()
+  void chat_client::do_read_header()
   {
     asio::async_read(socket_,
         asio::buffer(read_msg_.data(), chat_message::header_length),
@@ -79,7 +65,7 @@ private:
         });
   }
 
-  void do_read_body()
+  void chat_client::do_read_body()
   {
     asio::async_read(socket_,
         asio::buffer(read_msg_.body(), read_msg_.body_length()),
@@ -98,7 +84,7 @@ private:
         });
   }
 
-  void do_write()
+  void chat_client::do_write()
   {
     asio::async_write(socket_,
         asio::buffer(write_msgs_.front().data(),
@@ -119,49 +105,3 @@ private:
           }
         });
   }
-
-private:
-  asio::io_context& io_context_;
-  tcp::socket socket_;
-  chat_message read_msg_;
-  chat_message_queue write_msgs_;
-};
-
-int main(int argc, char* argv[])
-{
-  try
-  {
-    if (argc != 3)
-    {
-      std::cerr << "Usage: chat_client <host> <port>\n";
-      return 1;
-    }
-
-    asio::io_context io_context;
-
-    tcp::resolver resolver(io_context);
-    auto endpoints = resolver.resolve(argv[1], argv[2]);
-    chat_client c(io_context, endpoints);
-
-    std::thread t([&io_context](){ io_context.run(); });
-
-    char line[chat_message::max_body_length + 1];
-    while (std::cin.getline(line, chat_message::max_body_length + 1))
-    {
-      chat_message msg;
-      msg.body_length(std::strlen(line));
-      std::memcpy(msg.body(), line, msg.body_length());
-      msg.encode_header();
-      c.write(msg);
-    }
-
-    c.close();
-    t.join();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
-}
