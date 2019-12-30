da2794bd2c08d08f8f904cef148773ab281028c7
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-12-30 22:09:02 +0530

    some new files

---
+++ pi/global.cpp
@@ -0,0 +1,3 @@
+#include "global.h"
+
+int ser_dev = -1;
---
+++ pi/global.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/*File to keep track of all global variables*/
+
+/*Serial port used as global variable since read/write operations are done async*/
+extern int ser_dev;
